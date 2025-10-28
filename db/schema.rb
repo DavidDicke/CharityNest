a96f60403c06ea2f14b0d1646a3b76a983b33b75
David Dicke <170459648+DavidDicke@users.noreply.github.com>
2025-10-28 20:56:56 +0100

    migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,39 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_19_100833) do
+ActiveRecord::Schema[8.1].define(version: 2025_10_28_184147) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "action_text_rich_texts", force: :cascade do |t|
-    t.string "name", null: false
     t.text "body"
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
     t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.bigint "record_id", null: false
+    t.string "record_type", null: false
     t.datetime "updated_at", null: false
     t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
   end
 
   create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.bigint "record_id", null: false
+    t.string "record_type", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
     t.bigint "byte_size", null: false
     t.string "checksum"
+    t.string "content_type"
     t.datetime "created_at", null: false
+    t.string "filename", null: false
+    t.string "key", null: false
+    t.text "metadata"
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
@@ -53,106 +53,106 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_19_100833) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.text "description"
+    t.string "name"
+    t.datetime "updated_at", null: false
   end
 
   create_table "charities", force: :cascade do |t|
-    t.string "name"
-    t.bigint "user_id", null: false
     t.bigint "category_id", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "description"
+    t.string "name"
     t.string "partner"
-    t.string "youtube_id"
     t.text "teaser"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.string "youtube_id"
     t.index ["category_id"], name: "index_charities_on_category_id"
     t.index ["user_id"], name: "index_charities_on_user_id"
   end
 
   create_table "charity_projects", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.string "location"
     t.bigint "charity_id", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "goal"
-    t.float "latitude"
-    t.float "longitude"
+    t.text "description"
     t.text "description_aim"
-    t.text "description_steps"
     t.text "description_impact"
     t.text "description_scalability"
+    t.text "description_steps"
+    t.integer "goal"
+    t.float "latitude"
+    t.string "location"
+    t.float "longitude"
+    t.string "name"
+    t.datetime "updated_at", null: false
     t.index ["charity_id"], name: "index_charity_projects_on_charity_id"
   end
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
-    t.bigint "user_id", null: false
-    t.bigint "discussion_id", null: false
     t.datetime "created_at", null: false
+    t.bigint "discussion_id", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["discussion_id"], name: "index_comments_on_discussion_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "discussions", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.bigint "charity_project_id"
+    t.datetime "created_at", null: false
+    t.text "description"
     t.integer "donation_id"
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["charity_project_id"], name: "index_discussions_on_charity_project_id"
     t.index ["user_id"], name: "index_discussions_on_user_id"
   end
 
   create_table "donations", force: :cascade do |t|
-    t.boolean "recurrent"
-    t.bigint "charity_project_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.integer "amount_cents", default: 0, null: false
+    t.bigint "charity_project_id", null: false
     t.string "checkout_session_id"
+    t.datetime "created_at", null: false
+    t.boolean "recurrent"
     t.string "state"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["charity_project_id"], name: "index_donations_on_charity_project_id"
     t.index ["user_id"], name: "index_donations_on_user_id"
   end
 
   create_table "reports", force: :cascade do |t|
-    t.string "title"
-    t.text "teaser"
-    t.integer "report_type"
-    t.integer "score"
-    t.bigint "user_id", null: false
     t.bigint "charity_project_id", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "score_impact"
+    t.integer "report_type"
+    t.integer "score"
+    t.integer "score_adaptability"
     t.integer "score_communication"
     t.integer "score_efficiency"
-    t.integer "score_adaptability"
+    t.integer "score_impact"
+    t.text "teaser"
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["charity_project_id"], name: "index_reports_on_charity_project_id"
     t.index ["user_id"], name: "index_reports_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
     t.integer "role", default: 1
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
