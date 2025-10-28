bf52f88f00dc3e1914484a00e8e8b248e63a08ad
David Dicke <170459648+DavidDicke@users.noreply.github.com>
2025-10-28 20:49:03 +0100

    Revert "rails update"
    This reverts commit d2717b749670255ed35d184134366b14f593a044.

--- config/application.rb
+++ config/application.rb
@@ -8,13 +8,23 @@ Bundler.require(*Rails.groups)
 
 module CharityNest
   class Application < Rails::Application
+    config.action_controller.raise_on_missing_callback_actions = false if Rails.version >= "7.1.0"
+    config.generators do |generate|
+      generate.assets false
+      generate.helper false
+      generate.test_framework :test_unit, fixture: false
+    end
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
+
+    # Add app/services to the autoload paths
+    config.autoload_paths += %W(#{config.root}/app/services)
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
+    config.autoload_lib(ignore: %w(assets tasks))
 
     # Configuration for the application, engines, and railties goes here.
     #
